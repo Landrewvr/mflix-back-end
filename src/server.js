4191fe245b5a19d72ebccd33f13ac78d27a1ba7b
Landrewvr <luis.andrew.veras@gmail.com>
2022-10-22 15:21:53 -0400

    Serverless settings

---
+++ src/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const appPort = process.env.APP_PORT;
+
+app.use(express.json());
+app.use(cors());
+
+const moviesRouter = require('../routes/movies');
+
+// app.use('/movies', moviesRouter);
+app.use('/.netlify/functions/server', moviesRouter);
+
+// app.listen(appPort,() => console.log(`Listening to port ${appPort}`));
+
+module.exports=app;
+module.exports.handler = serverless(app);
