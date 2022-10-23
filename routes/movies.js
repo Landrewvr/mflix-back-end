d08b64ff6bb52a7c2e6ca7f00b2cda3a54cd0ac5
Landrewvr <luis.andrew.veras@gmail.com>
2022-10-23 03:14:46 -0400

    New config

--- routes/movies.js
+++ routes/movies.js
@@ -2,13 +2,6 @@ const express = require('express');
 const movie_mocks_db = require('./../assets/movie_mocks');
 const router = express.Router();
 
-router.all('*', (req,res,next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-})
-
 // Get all movies
 router.get('/', async (req,res) => {
     try{
@@ -21,6 +14,7 @@ router.get('/', async (req,res) => {
 
 // Get movies by genre
 router.get('/:genre', async (req,res) => {
+    res.header('Access-Control-Allow-Origin', '*');
     try{
         const result = await movie_mocks_db.findByGenre(req.params.genre);
         res.json(result);
