f24794f880f49dd3331dde8f3e1ce7156178b43b
Landrewvr <luis.andrew.veras@gmail.com>
2022-10-23 03:19:53 -0400

    test config

--- src/movies.js
+++ src/movies.js
@@ -7,11 +7,17 @@ const appPort = process.env.APP_PORT;
 
 app.use(express.json());
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 const moviesRouter = require('../routes/movies');
 
 // app.use('/movies', moviesRouter);
 app.use('/.netlify/functions/movies', moviesRouter);
+
 
 // app.listen(appPort,() => console.log(`Listening to port ${appPort}`));
 
