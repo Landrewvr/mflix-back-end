49c84a5a51f7856771b8feedf7a9ba328d111c3b
Landrewvr <luis.andrew.veras@gmail.com>
2022-10-23 03:09:15 -0400

    adding route configs

--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,13 @@ const express = require('express');
 const movie_mocks_db = require('./../assets/movie_mocks');
 const router = express.Router();
 
+router.all('*', (req,res,next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+
 // Get all movies
 router.get('/', async (req,res) => {
     try{
